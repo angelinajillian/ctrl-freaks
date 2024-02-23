ca37e5001330a236415095accd0953c202bd147f
iccraig <131424744+iccraig@users.noreply.github.com>
2024-02-22 17:18:59 -0800

    Added Player Health
    Player Health system implemented in character controller extended script, very basic damage taking system that has a cooldown of 2.0f, may want to make more robust in the future.

---
+++ Assets/Scripts/PlayerControllerExtended.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerExtended : MonoBehaviour
+{
+    [SerializeField] private int health = 3;
+    private bool canTakeDamage = true;
+    public float damageCooldown = 2.0f;
+
+    void Update()
+    {
+        if (!canTakeDamage)
+        {
+            damageCooldown -= Time.deltaTime;
+
+            if (damageCooldown <= 0.0f)
+            {
+                canTakeDamage = true;
+                damageCooldown = 2.0f;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") & canTakeDamage)
+        {
+            canTakeDamage = false;
+            Debug.Log($"Health: {health}");
+            health -= 1;
+            
+            if (health <= 0)
+            {
+                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
